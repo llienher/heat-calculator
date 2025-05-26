022138b0994e7af02d7ad11883f8318b32665663
Laurent Lienher <llienher.dev@gmail.com>
2025-05-26 17:14:57 +0200

    Refactor Card and CardManager

--- src/main/kotlin/common/CardManager.kt
+++ src/main/kotlin/common/CardManager.kt
@@ -1,7 +1,72 @@
 package common
 
+class CardManager(players: List<Player>) {
+    var playersMoves: MutableList<PlayerMoveInfo> = ArrayList()
+    var possibleMoves: MutableList<PossiblePlayerMoveInfo> = ArrayList()
+    var zoneQuotas = emptyList<Int>()
 
-class CardManager() {
-    val cardPile = arrayListOf<Card>()
-    val discardPile = arrayListOf<Card>()
+    // val allMoves: IntArray = (10..19).toIntArray()
+
+    init {
+        // Set the quotas related to the number of CPUs
+        zoneQuotas = if (players.size <= 6) {
+            listOf(2, 3, 3, 2)
+        } else {
+            listOf(2,4,4,2)
+        }
+        // Set a list of usable moves for each player
+        for (player in players) {
+            possibleMoves.add(PossiblePlayerMoveInfo(player.color, createMoveInfoList()))
+        }
+    }
+
+    fun createOnePlayerMove(color: PlayerColor): PlayerMoveInfo {
+        // Get the possible move list for this player
+        val possibilities = possibleMoves.find { it.color == color }
+        if (possibilities === null) {
+            throw Exception("Not possible to generate player move.")
+        }
+
+        val randomNumber = (1..possibilities.possibleMoveInfo.size).random()
+        val move = PlayerMoveInfo(color, possibilities.possibleMoveInfo[randomNumber])
+        possibilities.possibleMoveInfo.removeAt(randomNumber)
+        return move
+    }
+
+    fun createOneCard(players: List<Player>): Card {
+        for (player in players) {
+            val playerMove = createOnePlayerMove(player.color)
+            playersMoves.add(playerMove)
+        }
+        return Card(playersMoves)
+    }
+
+    fun createMoveInfoList(): ArrayList<MoveInfo> {
+        val miList = arrayListOf<MoveInfo>()
+        for (move in 10..19) {
+            val zone = getZoneByMove(move)
+            miList.add(MoveInfo(zone, move))
+        }
+        return miList
+    }
+
+    private fun getZoneByMove(move: Int): Int {
+        return when (move) {
+            in 10..11 -> 0
+            in 12..14 -> 1
+            in 14..17 -> 2
+            in 18..19 -> 3
+            else -> error("Move is out of range to get zone.")
+        }
+    }
 }
+
+data class Card(val moves: MutableList<PlayerMoveInfo>)
+
+data class MoveInfo(var zone: Int, var move: Int) {
+    companion object {} // For tests
+}
+
+data class PossiblePlayerMoveInfo(val color: PlayerColor, var possibleMoveInfo: MutableList<MoveInfo>)
+
+data class PlayerMoveInfo(val color: PlayerColor, val moveInfo: MoveInfo)
--- src/test/kotlin/common/CardManagerTest.kt
+++ src/test/kotlin/common/CardManagerTest.kt
@@ -2,13 +2,20 @@ package common
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.test.assertContains
 
-class CardTest {
+class CardManagerTest {
     private val player1 = Player(PlayerColor.RED, PlayerType.NPC)
     private val player2 = Player(PlayerColor.BLUE, PlayerType.NPC)
     private val playersList = listOf(player1, player2)
-    private val testCard = Card(playersList)
+    private val testCardManager = CardManager(playersList)
 
+
+    @Test
+    fun `Create a quota for 6 or less players`() {
+        val excepted: ArrayList<Int> = arrayListOf(2,3,3,2)
+        assertEquals(excepted, testCardManager.zoneQuotas)
+    }
 
     @Test
     fun `Create an array of all move possibilities`() {
@@ -24,7 +31,15 @@ class CardTest {
             MoveInfo(3, 18),
             MoveInfo(3, 19),
         )
-        assertEquals(excepted, testCard.createCombinations())
+        assertEquals(excepted, testCardManager.createMoveInfoList())
     }
 
+    @Test
+    fun `Create a player move`() {
+        val color = PlayerColor.RED
+        val current = testCardManager.createOnePlayerMove(color)
+        val exceptedPossible = testCardManager.possibleMoves[0].possibleMoveInfo
+        assertEquals(9, exceptedPossible.size)
+        assert(current.moveInfo !in exceptedPossible)
+    }
 }
